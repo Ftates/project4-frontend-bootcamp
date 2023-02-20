776628ff2c5ba4adbba1b77e3b89103edf43ca9e
TheConfusedDuck <lj.michaelhung@gmail.com>
2023-02-20 12:47:00 +0800

    changing branch

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,86 +1,9 @@
-// import React from "react";
-// import { useState, useEffect } from "react";
-// import { Line } from "react-chartjs-2";
+import React from "react";
+import { Bar } from "react-chartjs-2";
+import { Chart as ChartJS } from "chart.js/auto";
 
-// export default function BarChart() {
-//   const [chartData, setChartData] = useState({});
+function BarChart({ chartData }) {
+  return <Bar data={chartData} />;
+}
 
-//   useEffect(() => {
-//     setChartData({
-//       labels: [
-//         "2020-12-12",
-//         "2020-12-13",
-//         "2020-12-14",
-//         "2020-12-16",
-//         "2020-12-17", // dates
-//         "2020-12-18",
-//         "2020-12-27",
-//         "2020-12-29",
-//         "2021-01-02",
-//       ],
-//       datasets: [
-//         {
-//           label: "Weight (lbs)",
-//           data: [
-//             196,
-//             196,
-//             197,
-//             198,
-//             195, // weights
-//             196,
-//             203,
-//             200,
-//             201,
-//           ],
-//           fill: false,
-//           lineTension: 0,
-//           backgroundColor: "#2085d8",
-//           borderColor: "#2085d8",
-//           borderCapStyle: "butt",
-//           borderDash: [],
-//           borderDashOffset: 0.0,
-//           borderJoinStyle: "miter",
-//           pointBorderColor: "#2085d8",
-//           pointBackgroundColor: "#FFF",
-//           pointBorderWidth: 1,
-//           pointHoverRadius: 5,
-//           pointHoverBackgroundColor: "#2f3640",
-//           pointHoverBorderColor: "#2085d8",
-//           pointHoverBorderWidth: 2,
-//           pointRadius: 1,
-//           pointHitRadius: 10,
-//         },
-//       ],
-//     });
-//   }, []);
-
-//   return (
-//     <div className="chart">
-//       <Line
-//         data={chartData}
-//         options={{
-//           responsive: true,
-//           title: { text: "DAILY WEIGHT TREND", display: true },
-//           scales: {
-//             yAxes: [
-//               {
-//                 scaleLabel: {
-//                   display: true,
-//                   labelString: "Weight(lbs)",
-//                 },
-//               },
-//             ],
-//             xAxes: [
-//               {
-//                 scaleLabel: {
-//                   display: true,
-//                   labelString: "Dates",
-//                 },
-//               },
-//             ],
-//           },
-//         }}
-//       />
-//     </div>
-//   );
-// }
+export default BarChart;
--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import "./Dashboard.css";
 import Sidebar from "../../components/sidebar";
 import BarChart from "../../components/BarChart";
+import LineChart from "../../components/LineChart";
 
 export default function Dashboard() {
   const [txnData, setTxnData] = useState([]);
@@ -48,14 +49,60 @@ export default function Dashboard() {
     console.log("Wallet data", walletData);
   }, [walletData]);
 
+  const UserData = [
+    {
+      id: 1,
+      year: 2016,
+      userGain: 80000,
+      userLost: 823,
+    },
+    {
+      id: 2,
+      year: 2017,
+      userGain: 45677,
+      userLost: 345,
+    },
+    {
+      id: 3,
+      year: 2018,
+      userGain: 78888,
+      userLost: 555,
+    },
+    {
+      id: 4,
+      year: 2019,
+      userGain: 90000,
+      userLost: 4555,
+    },
+    {
+      id: 5,
+      year: 2020,
+      userGain: 4300,
+      userLost: 234,
+    },
+  ];
+
+  const [userData, setUserData] = useState({
+    labels: UserData.map((data) => data.year),
+    datasets: [
+      {
+        label: "Users Gained",
+        data: UserData.map((data) => data.userGain),
+      },
+    ],
+  });
+
   return (
     <div className="Screen">
-      <div className="sidebar">
-        <Sidebar />
+      <div className="dashboard">
+        <div className="growthChart">
+          <LineChart chartData={userData} />
+          <LineChart chartData={userData} />
+        </div>
+        <div className="overview">
+          <LineChart chartData={userData} />
+        </div>
       </div>
-
-      <span className="header">Dashboard</span>
-      {/* <BarChart /> */}
     </div>
   );
 }
