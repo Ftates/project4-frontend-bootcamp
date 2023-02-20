092558d4804af2c48705c70d25c4195f09e97def
TheConfusedDuck <lj.michaelhung@gmail.com>
2023-02-21 01:06:12 +0800

    Missing user auth part, submitting form and retrieving wallet id

--- src/pages/Transaction Log/Transaction.js
+++ src/pages/Transaction Log/Transaction.js
@@ -5,7 +5,6 @@ import "./Transaction.css";
 
 import TransactionTable from "../../components/TransactionTable";
 import Button from "@mui/material/Button";
-import Backdrop from "@mui/material/Backdrop";
 import Dialog from "@mui/material/Dialog";
 import DialogTitle from "@mui/material/DialogTitle";
 import DialogActions from "@mui/material/DialogActions";
@@ -15,9 +14,22 @@ import DialogContent from "@mui/material/DialogContent";
 import Box from "@mui/material/Box";
 import InputLabel from "@mui/material/InputLabel";
 import FormControl from "@mui/material/FormControl";
-import NativeSelect from "@mui/material/NativeSelect";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+
+import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
 export default function Transactions() {
+  const [open, setOpen] = useState(false);
+  const [date, setDate] = useState();
+  const [wallet, setWallet] = useState("");
+  const [coin, setCoin] = useState("");
+  const [type, setType] = useState("");
+  const [quantity, setQuantity] = useState(0);
+  const [price, setPrice] = useState(0);
+
   const [newTxn, setNewTxn] = useState({});
 
   const [userWallets, setUserWallets] = useState([]);
@@ -35,12 +47,6 @@ export default function Transactions() {
     getUserWallet();
   }, []);
 
-  // useEffect(() => {
-  //   console.log(userWallets);
-  // }, [userWallets]);
-
-  const [open, setOpen] = useState(false);
-
   const handleClickOpen = () => {
     setOpen(true);
   };
@@ -49,45 +55,152 @@ export default function Transactions() {
     setOpen(false);
   };
 
+  const handleChangeForm = (e) => {
+    if (e.target.name === "wallet") {
+      setWallet(e.target.value);
+    } else if (e.target.name === "coin") {
+      setCoin(e.target.value);
+    } else if (e.target.name === "type") {
+      setType(e.target.value);
+    } else if (e.target.name === "quantity") {
+      setQuantity(e.target.value);
+    } else if (e.target.name === "price") {
+      setPrice(e.target.value);
+    }
+  };
+
+  const handleSubmitForm = (e) => {
+    if (date && wallet && coin && type && quantity && price) {
+      setNewTxn({
+        date: date,
+        wallet: wallet,
+        coin: coin,
+        type: type,
+        quantity: quantity,
+        price: price,
+      });
+      setOpen(false);
+      setWallet("");
+      setCoin("");
+      setType("");
+      setQuantity(0);
+      setPrice(0);
+    } else {
+      console.log("Error");
+    }
+  };
+
   return (
     <div className="Screen">
       <div>
         <Button variant="contained" onClick={handleClickOpen}>
           + New Transaction
         </Button>
-        <Dialog open={open} onClose={handleClose}>
-          <DialogTitle>Add Transaction</DialogTitle>
-          <DialogContent>
-            <form>
-              <label>Date</label>
 
-              <Box sx={{ minWidth: 120 }}>
-                <FormControl fullWidth>
-                  <InputLabel variant="standard" htmlFor="uncontrolled-native">
-                    Age
-                  </InputLabel>
-                  <NativeSelect
-                    defaultValue={30}
-                    inputProps={{
-                      name: "age",
-                      id: "uncontrolled-native",
+        <Dialog maxWidth={"lg"} open={open} onClose={handleClose}>
+          <DialogTitle>Add Transaction</DialogTitle>
+
+          <DialogContent style={{ maxHeight: "400px" }}>
+            <form className="form">
+              <Box sx={{ minWidth: 120, marginLeft: 2, marginRight: 2 }}>
+                <LocalizationProvider dateAdapter={AdapterMoment}>
+                  <DatePicker
+                    label="Date"
+                    value={date}
+                    onChange={(newValue) => {
+                      console.log(newValue);
+                      setDate(newValue._d);
                     }}
+                    renderInput={(params) => <TextField {...params} />}
+                  />
+                </LocalizationProvider>
+              </Box>
+
+              <Box sx={{ minWidth: 120, marginLeft: 2, marginRight: 2 }}>
+                <FormControl fullWidth>
+                  <InputLabel id="demo-simple-select-standard-label">
+                    Wallet
+                  </InputLabel>
+                  <Select
+                    labelId="demo-simple-select-standard-label"
+                    id="demo-simple-select-standard"
+                    value={wallet}
+                    label="Wallet"
+                    name="wallet"
+                    onChange={handleChangeForm}
                   >
-                    {userWallets.map((data) => {
-                      <option value={data.name}>${data.name}</option>;
-                    })}
-                  </NativeSelect>
+                    {userWallets.map((data) => (
+                      <MenuItem key={`${data.name}`} value={data.name}>
+                        {data.name}
+                      </MenuItem>
+                    ))}
+                  </Select>
                 </FormControl>
               </Box>
-              <label>Type</label>
-              <label>Coin</label>
-              <label>Quantity</label>
-              <label>Price</label>
+
+              <Box sx={{ minWidth: 120, marginLeft: 2, marginRight: 2 }}>
+                <FormControl fullWidth>
+                  <InputLabel id="demo-simple-select-standard-label">
+                    Type
+                  </InputLabel>
+                  <Select
+                    labelId="demo-simple-select-standard-label"
+                    id="demo-simple-select-standard"
+                    value={type}
+                    label="Type"
+                    name="type"
+                    onChange={handleChangeForm}
+                  >
+                    <MenuItem value="Buy">Buy</MenuItem>
+                    <MenuItem value="Sell">Sell</MenuItem>
+                    <MenuItem value="Deposit">Deposit</MenuItem>
+                  </Select>
+                </FormControl>
+              </Box>
+
+              <TextField
+                sx={{ marginLeft: 2, marginRight: 2 }}
+                label="Coin"
+                name="coin"
+                value={coin}
+                onChange={handleChangeForm}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                variant="standard"
+              />
+
+              <TextField
+                sx={{ marginLeft: 2, marginRight: 2 }}
+                id="standard-number"
+                label="Quantity"
+                type="number"
+                name="quantity"
+                value={quantity}
+                onChange={handleChangeForm}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                variant="standard"
+              />
+              <TextField
+                sx={{ marginLeft: 2, marginRight: 2 }}
+                id="standard-number"
+                label="Price"
+                type="number"
+                name="price"
+                value={price}
+                onChange={handleChangeForm}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                variant="standard"
+              />
             </form>
           </DialogContent>
           <DialogActions>
             <Button onClick={handleClose}>Cancel</Button>
-            <Button onClick={handleClose}>Submit</Button>
+            <Button onClick={handleSubmitForm}>Submit</Button>
           </DialogActions>
         </Dialog>
       </div>
